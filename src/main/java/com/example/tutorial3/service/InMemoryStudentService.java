7c3c852cc07d9e771dc3629213308e765c861864
budi.wahyu61 <budi.wahyu61>
2018-02-27 20:46:30 +0700

    Initial Commit APAP Latihan 3

---
+++ src/main/java/com/example/tutorial3/service/InMemoryStudentService.java
@@ -0,0 +1,44 @@
+package com.example.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.example.tutorial3.model.StudentModel;
+
+public class InMemoryStudentService implements StudentService{
+	private static List<StudentModel> studentList = new ArrayList<StudentModel>();
+	
+	@Override
+	public StudentModel selectStudent(String npm) {
+		StudentModel student = findStudent(npm);
+		if(student != null) {
+			return student;
+		}
+		return null;
+		
+	}
+	
+	public StudentModel findStudent(String npm) {
+		StudentModel student = null;
+		for(StudentModel sl : studentList) {
+			if(sl.getNpm().equals(npm)) {
+				student = sl;
+				break;
+			}
+			
+		}
+		return student;
+		
+	}
+
+	@Override
+	public List<StudentModel> selectAllStudents() {
+		return studentList;
+	}
+
+	@Override
+	public void addStudent(StudentModel student) {
+		studentList.add(student);		
+	}
+}
